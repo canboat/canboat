bf06504aee85bc08f254f3214cf392c6c5df5661
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-03-06 15:42:31 +0200

    Write PGNs to CAN socket, support both single & fast packet formats

--- socketcan-writer/socketcan-writer.c
+++ socketcan-writer/socketcan-writer.c
@@ -32,6 +32,9 @@ along with CANboat.  If not, see <http://www.gnu.org/licenses/>.
 #include "pgn.h"
 
 static int openCanDevice(char * device, int * socket);
+static void writeRawPGNToCanSocket(RawMessage* msg, int socket);
+static void sendCanFrame(struct can_frame* frame, int socket);
+static void sendN2kFastPacket(RawMessage* msg, struct can_frame* frame, int socket);
 
 
 int main(int argc, char ** argv)
@@ -58,7 +61,7 @@ int main(int argc, char ** argv)
     {
       continue;  // Parsing failed -> skip the line
     }
-    printf("Read: %s, %d, %d, %d, %d, %d\n", m.timestamp, m.prio, m.pgn, m.src, m.dst, m.len);
+    writeRawPGNToCanSocket(&m, socket);
   }
 
   close(socket);
@@ -98,4 +101,68 @@ static int openCanDevice(char * device, int * canSocket)
   }
 
   return 0;
+}
+
+static void writeRawPGNToCanSocket(RawMessage * msg, int socket)
+{
+  struct can_frame frame;
+  memset(&frame, 0, sizeof(frame));
+  frame.can_id = getCanIdFromISO11783Bits(msg->prio, msg->pgn, msg->src, msg->dst);
+
+  if(msg->len <= 8)  // 8 or less bytes of data -> PGN fits into a single CAN frame
+  {
+    frame.can_dlc = msg->len;
+    memcpy(frame.data, msg->data, msg->len);
+    sendCanFrame(&frame, socket);
+  }
+  else
+  {           // Send PGN as n2k fast packet (spans multiple CAN frames, but CAN ID is still same for each frame)
+    sendN2kFastPacket(msg, &frame, socket);
+  }
+}
+
+static void sendCanFrame(struct can_frame * frame, int socket)
+{
+  if (write(socket, frame, sizeof(struct can_frame)) != sizeof(struct can_frame))
+  {
+    perror("write");
+  }
+}
+
+/*
+  See pgn.h for n2k fast packet data format
+*/
+static void sendN2kFastPacket(RawMessage * msg, struct can_frame* frame, int socket)
+{
+  int index = 0;
+  int remainingDataBytes = msg->len;
+  while(remainingDataBytes > 0)
+  {
+    frame->data[0] = index;  // fast packet index (increases by 1 for every CAN frame), 'order' (the 3 uppermost bits) is left as 0 for now
+
+    if(index == 0)    // 1st frame
+    {
+      frame->data[1] = msg->len;  // fast packet payload size
+      memcpy(frame->data + 2, msg->data, 6);  // 6 first data bytes
+      frame->can_dlc = 8;
+      remainingDataBytes -= 6;
+    }
+    else              // further frames
+    {
+      if(remainingDataBytes > 7)
+      {
+        memcpy(frame->data + 1, msg->data + 6 + (index-1) * 7, 7);  // 7 next data bytes
+        frame->can_dlc = 8;
+        remainingDataBytes -= 7;
+      }
+      else
+      {
+        memcpy(frame->data + 1, msg->data + 6 + (index-1) * 7, remainingDataBytes);  // 7 next data bytes
+        frame->can_dlc = 1 + remainingDataBytes;
+        remainingDataBytes = 0;
+      }
+    }
+    sendCanFrame(frame, socket);
+    index++;
+  }
 }
