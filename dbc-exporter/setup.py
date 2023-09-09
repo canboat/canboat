ffce1ada8b0af6dff3e696c44188177143e228ec
Kees Verruijt <kees@verruijt.net>
2023-09-09 16:17:17 +0200

    Fix cantools library to v38.0.0
    This fixes the build errors, we don't need anything newer so just
    fix version to (one of the) last known good ones.

--- dbc-exporter/setup.py
+++ dbc-exporter/setup.py
@@ -67,7 +67,7 @@ setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'cantools'
+        'cantools==38.0.0'
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
